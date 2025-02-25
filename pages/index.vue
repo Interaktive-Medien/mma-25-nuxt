9db9b6d4e8834dfbcf627de2f18fc615a06e3d67
Nick Schneeberger <nick.schneeberger@icloud.com>
2025-02-25 11:42:49 +0100

    update

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,9 @@
   <div class="p-16 bg-green-100 w-full md:w-2/3 xl:w-1/2 mx-auto">
     <h1 class="">Ich bin die Startseite</h1>
 
-    <p>Ich bin ein Paragraph</p>
+    <p :class="{ 'text-green-500': isActive, 'text-red-500': !isActive }">
+      Momentan habe ich so viele Todos: {{ numberOfTodos }}
+    </p>
 
     <InputTodo @addTodo="addTodo" />
     <TodoList :todos="todos" />
@@ -13,9 +15,14 @@
 console.log("Ich bin die Startseite");
 
 const todos = ref([]);
+const isActive = ref(true);
 
 const addTodo = (todo) => {
   todos.value.push(todo);
   console.log(todos.value);
 };
+
+const numberOfTodos = computed(() => {
+  return todos.value.length * 2;
+});
 </script>
