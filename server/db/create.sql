b167ce2e155217fd52f748fa324e0cbf07ca2376
Nick Schneeberger <nick.schneeberger@icloud.com>
2025-05-19 13:35:49 +0200

    add posts

--- server/db/create.sql
+++ server/db/create.sql
@@ -3,3 +3,8 @@ CREATE TABLE posts (
   title TEXT NOT NULL,
   content TEXT
 );
+
+INSERT INTO posts (title, content) VALUES
+  ('Post 1', 'Das ist der erste Inhalt.'),
+  ('Post 2', 'Ein weiterer Post.'),
+  ('Post 3', 'Noch ein Beispieltext.');
