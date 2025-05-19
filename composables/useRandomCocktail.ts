33bb6fc3d528f55af55f6e6feac12c0972c90a9e
Nick Schneeberger <nick.schneeberger@icloud.com>
2025-05-19 10:16:20 +0200

    add ingredients

--- composables/useRandomCocktail.ts
+++ composables/useRandomCocktail.ts
@@ -2,6 +2,21 @@ export interface Cocktail {
   strDrink: string;
   strInstructions: string;
   strDrinkThumb: string;
+  strIngredient1?: string;
+  strIngredient2?: string;
+  strIngredient3?: string;
+  strIngredient4?: string;
+  strIngredient5?: string;
+  strIngredient6?: string;
+  strIngredient7?: string;
+  strIngredient8?: string;
+  strIngredient9?: string;
+  strIngredient10?: string;
+  strIngredient11?: string;
+  strIngredient12?: string;
+  strIngredient13?: string;
+  strIngredient14?: string;
+  strIngredient15?: string;
 }
 
 export const useRandomCocktail = async () => {
@@ -14,5 +29,22 @@ export const useRandomCocktail = async () => {
   }
 
   // Always return the first drink (only one is returned by this endpoint)
-  return data.value?.drinks?.[0] ?? null;
+  const cocktail = data.value?.drinks?.[0] ?? null;
+
+  if (cocktail) {
+    // Transform ingredients into an array, filtering out null/undefined values
+    const ingredients = Object.entries(cocktail)
+      .filter(([key, value]) => key.startsWith("strIngredient") && value)
+      .map(([_, value]) => value as string);
+
+    // Return only the properties we want, with ingredients as an array
+    return {
+      strDrink: cocktail.strDrink,
+      strInstructions: cocktail.strInstructions,
+      strDrinkThumb: cocktail.strDrinkThumb,
+      ingredients,
+    };
+  }
+
+  return null;
 };
---
+++ pages/cocktail.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-1/2 mx-auto">
+    <h1>{{ cocktail.strDrink }}</h1>
+
+    <img class="w-full" :src="cocktail.strDrinkThumb" alt="" />
+
+    <p class="mt-4">{{ cocktail.strInstructions }}</p>
+  </div>
+</template>
+
+<script setup>
+const cocktail = await useRandomCocktail();
+console.log(cocktail);
+</script>
